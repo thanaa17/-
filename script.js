201d8136fc59b5c3a29744238749873d1f7c0c8b
thanaa17 <tahathanaa194@gmail.com>
2024-12-12 12:43:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendValue(value) {
+    const display = document.getElementById("display");
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById("display");
+    display.value = "";
+}
+
+function calculate() {
+    const display = document.getElementById("display");
+    try {
+        display.value = eval(display.value); // Note: Use eval carefully!
+    } catch (error) {
+        display.value = "Error!";
+    }
+}
